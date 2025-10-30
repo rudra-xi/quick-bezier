14abb8437eed7f5303defe67972636accd3b8fd6
rudra <goutam.prasad.2707@gmail.com>
2025-10-30 23:13:38 +0530

    refactor: simplify Heading component by removing unused props and enhancing structure

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,64 +1,23 @@
 import React from "react";
-import { github, linkedin, instagram } from "../Assets";
 
-function Heading({ heading, tips }) {
+/**
+ * Heading component displays a primary heading and a tip for the user.
+ *
+ * @param {object} props - The component props.
+ * @param {string} props.heading - The main text to display as the heading.
+ * @returns {JSX.Element} A heading section for a page.
+ */
+function Heading({ heading }) {
 	return (
 		<>
-			<div className="flex sm:flex-row sm:text-left text-center flex-col sm:gap-56 gap-4">
-				<h1 className="sm:text-4xl text-2xl font-black sm:w-[55%]">
-					{heading}
-				</h1>
-
-				<div className="flex flex-col gap-2">
-					<div className="flex flex-col">
-						<p>&copy; 2025 Rudra-Xi. All rights reserved.</p>
-						<p className="">Built with ♡ by Rudra-Xi.</p>
-					</div>
-
-					<div className="flex gap-5 sm:justify-start justify-center">
-						<a
-							href="https://github.com/rudra-xi"
-							target="_blank"
-							rel="noopener noreferrer"
-							className="hover:scale-105 hover:rotate-6 duration-300 transition-all"
-						>
-							<img
-								src={github}
-								alt="github"
-								className="w-8"
-							/>
-						</a>
-						<a
-							href="https://www.linkedin.com/in/goutam-rudraxi/"
-							target="_blank"
-							rel="noopener noreferrer"
-							className="hover:scale-105 hover:rotate-6 duration-300 transition-all"
-						>
-							<img
-								src={linkedin}
-								alt="linkedin"
-								className="w-8"
-							/>
-						</a>
-						<a
-							href="https://www.instagram.com/rudra.xii/"
-							target="_blank"
-							rel="noopener noreferrer"
-							className="hover:scale-105 hover:rotate-6 duration-300 transition-all"
-						>
-							<img
-								src={instagram}
-								alt="instagram"
-								className="w-8"
-							/>
-						</a>
-					</div>
-				</div>
+			<div className="flex text-center flex-col gap-4">
+				<h1 className="text-4xl font-black">{heading}</h1>
 			</div>
-			<div className="py-5">
-				<p className="flex text-sm">
+			<div className="pt-5 pb-10">
+				<p className="flex items-center justify-center text-sm">
 					<span className="font-black">Tips: &nbsp;</span>
-					{tips}
+					Hover on any animation to preview it and copy the CSS
+					code effortlessly!
 				</p>
 			</div>
 		</>
