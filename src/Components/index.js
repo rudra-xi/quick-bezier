53f9ca0263c94c7074e1b5fdc8a75a2a897b951e
rudra <goutam.prasad.2707@gmail.com>
2025-10-30 23:13:57 +0530

    feat: add Footer component to exports in index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Heading from "./Heading";
+import Footer from "./Footer";
 import BezierCard from "./BezierCard";
 
-export { Navbar, Heading, BezierCard };
+export { Navbar, Heading, BezierCard, Footer };
